ce3ed9ace87ad43955d04fb82d71283a2fc3b09a
yuu10 <yuu10@DESKTOP-CK56FOC>
2018-01-14 20:11:58 +0900

    graph2

--- src/graph/Graph_view.java
+++ src/graph/Graph_view.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -31,13 +33,32 @@ public class Graph_view  extends Frame implements ActionListener,WindowListener{
 		button2.addActionListener(this);
         DefaultCategoryDataset data = new DefaultCategoryDataset();
 		
-		data.addValue(300, "USA", "2005");
+		/*data.addValue(300, "USA", "2005");
 		data.addValue(500, "USA", "2006");
 		data.addValue(120, "USA", "2007");
 		
 		data.addValue(200, "China", "2005");
 		data.addValue(400, "China", "2006");
-		data.addValue(320, "China", "2007");
+		data.addValue(320, "China", "2007");*/
+        
+        int ton;
+        String name,year;
+        ResultSet rs;
+        MySQL mysql = new MySQL();
+        rs = mysql.selectAll();
+        
+        try {
+        	while(rs.next()) {
+        		name  = rs.getString("name");
+        		year = rs.getString("year");
+        		ton = rs.getInt("ton");
+        		data.addValue(ton, name, year);
+        	}
+        }catch(SQLException e) {
+        	//TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
+        
 		
 		if(i == 0) {
 			JFreeChart chart = ChartFactory.createLineChart(
